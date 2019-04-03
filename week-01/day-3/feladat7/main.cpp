c6c54b6c3c4b6831facdf52449cd7d290fcde21c
Kata <Kata@1743f.w.time4vps.cloud>
2019-04-03 16:09:02 +0300

    gitignore

---
+++ week-01/day-3/feladat7/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    int currentHours = 14;
+    int currentMinutes = 51;
+    int currentSeconds = 30;
+    int remainingSeconds = 24*60*60 - (14*60 + 51*60 + 30);
+
+    std::cout << remainingSeconds << std:: endl;
+
+return 0;
+}
