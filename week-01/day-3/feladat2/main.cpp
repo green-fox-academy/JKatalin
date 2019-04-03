c93100a0f97afb6a4bc12d308275be7b78313c29
Kata <Kata@1743f.w.time4vps.cloud>
2019-04-03 14:02:42 +0300

    day3-feladat1-2

---
+++ week-01/day-3/feladat2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << 6 * 5 * 17 << std::endl;
+    std::cout << 6 * 5 * 17 * 100 / (52 * 17) << std::endl;
+
+    std::cout << "number project" << std::endl;
+    std::cout << 13 + 22 << std::endl;
+    std::cout << 22 - 13 << std::endl;
+    std::cout << 22 * 13 << std::endl;
+    std::cout << 22.0 / 13.0 << std::endl;
+    std::cout << 22 / 13 << std::endl;
+    std::cout << 22 % 13 << std::endl;
+
+    std::cout << "about me" << std::endl;
+    std::cout << "Kata" << std::endl;
+    std::cout << 32 << std::endl;
+    std::cout << 1.67 << std::endl;
+
+
+
+    return 0;
+}
