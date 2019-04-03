8020ce89919079fd4d5e1cb83dac767478363f80
Kata <Kata@1743f.w.time4vps.cloud>
2019-04-03 14:47:59 +0300

    feladat5

---
+++ week-01/day-3/feladat5/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::string myName = "Kata";
+    int myAge = 25;
+    double myHeight = 1.67;
+    bool IamMarried = true;
+
+    std::cout << myName << std::endl;
+    std::cout << myAge << std::endl;
+    std::cout << myHeight << std::endl;
+    std::cout << IamMarried << std::endl;
+
+
+    return 0;
+}
