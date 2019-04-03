bb6f91ee35f750580125f2cdf2576093f2cf9ca9
Kata <Kata@1743f.w.time4vps.cloud>
2019-04-03 18:00:36 +0300

    feladat-13

---
+++ week-01/day-3/feladat13/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    int ch = 0;
+    while (ch < 100) {
+        std::cout << "I won't cheat on the exam!" << std::endl;
+        ch += 1;
+    }
+
+    int ch2 = 0;
+    while (ch2 < 10) {
+        std::cout << "I  really won't cheat on the exam!" << std::endl;
+        ch2++;
+    }
+
+    int ch3 = 0;
+    while (ch3 < 5) {
+        std::cout << "I totally won't cheat on the exam!" << std::endl;
+        ch3 += 1;
+    }
+    return 0;
+}
