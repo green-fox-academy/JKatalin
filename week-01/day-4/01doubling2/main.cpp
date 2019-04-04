2185770d945ac50c6790bcadd35d3966332941c5
Kata <Kata@1743f.w.time4vps.cloud>
2019-04-04 13:46:09 +0300

    doubling-2verzio-returnnel

--- week-01/day-4/01doubling2/main.cpp
+++ week-01/day-4/01doubling2/main.cpp
@@ -7,7 +7,7 @@ int doubling(int a) {
 
 int main(int argc, char *args[]) {
     int baseNum = 123;
-    std::cout << "result of doubling" << doubling(baseNum) << std::endl;
+    std::cout << "result of doubling " << doubling(baseNum) << std::endl;
 
     // - Create an integer variable named `baseNum` and assign the value `123` to it
     // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
@@ -15,4 +15,3 @@ int main(int argc, char *args[]) {
 
     return 0;
 }
-
