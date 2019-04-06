33984682d8169f5cdb66c424b5eecef3b44499ee
Kata <Kata@1743f.w.time4vps.cloud>
2019-04-06 16:55:45 +0300

    week-01 day-4 increment elements add

---
+++ week-01/day-4/11increment-element/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    int t[] = {1, 2, 3, 4, 5};
+
+    t[2] = t[2] + 4;
+    std::cout << "The incremented third element is: " << t[2]<< std::endl;
+
+    return 0;
+}
+
+    // - Create an array variable named `t`
+    //    with the following content: `[1, 2, 3, 4, 5]`
+    // - Increment the third element
+    // - Print the third element
