00c49e19fb3434408c1fdbe93702b4f1733c537c
Kata <Kata@1743f.w.time4vps.cloud>
2019-04-09 16:15:22 +0300

    week-02/day2 List introduction1: added

---
+++ week-02/day-2/06list-introduction1/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    std::vector<std::string> nameVector;
+
+    std::cout << nameVector.size() << std::endl;
+
+    nameVector.insert(nameVector.begin(), "William");
+
+    if (nameVector.empty()) {
+        std::cout << "nameVector is empty" << std::endl;
+    } else {
+        std::cout << "nameVector is not empty" << std::endl;
+    }
+
+    nameVector.push_back("John");
+    nameVector.insert(nameVector.begin() + 2, "Amanda");
+
+    std::cout << nameVector.size() << std::endl;
+
+    std::cout << nameVector[2] << std::endl;
+
+    std::vector<std::string>::iterator itName = nameVector.begin();
+    int i = 0;
+    for (; itName != nameVector.end(); itName++) {
+        // std::cout << *itName << std::endl;
+        
+        std::cout << i + 1 << ". " << *itName << std::endl;
+        i++;
+    }
+
+    nameVector.erase(nameVector.begin() + 1);
+
+    std::vector<std::string>::iterator itNameRev = nameVector.end();
+    for (; itNameRev != nameVector.begin() - 1; itNameRev--) {
+        std::cout << *itNameRev << std::endl;
+    }
+
+    nameVector.clear();
+
+
+    for (int i = 0; i < nameVector.size(); i++) {
+        std::cout << nameVector.at(i);
+    }
+
+    return 0;
+}
