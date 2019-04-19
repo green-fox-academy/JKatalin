f7b2c6ddf19060ba8adaa9775c4b64a9adc6b8f1
Kata <Kata@1743f.w.time4vps.cloud>
2019-04-19 15:48:45 +0200

    week-02/day2 List inroduction2: modified, finished

--- week-02/day-2/08list-introduction2/main.cpp
+++ week-02/day-2/08list-introduction2/main.cpp
@@ -21,15 +21,52 @@ int main() {
     }
 
 
-    listB.erase(listB.begin()+3);
+    listB.erase(listB.begin() + 3);
 
-    listA.insert(listA.begin()+4, "Kiwifruit");
+    listA.insert(listA.begin() + 4, "Kiwifruit");
 
-    if (listA.size()>listB.size()){
-        std::cout<< "List A is bigger, than List B"<<std::endl;
+    if (listA.size() > listB.size()) {
+        std::cout << "List A is bigger, than List B" << std::endl;
+    } else if (listA.size() < listB.size()) {
+        std::cout << "List B is bigger, than List A" << std::endl;
+    } else {
+        std::cout << "List A is equal with List B" << std::endl;
     }
 
 
+    bool avocadoInList = false;
+    for (int i = 0; i < listA.size(); i++) {
+        if (listA.at(i) == "Avocado") {
+            std::cout << "The index of Avocado in list A: " << i << std::endl;
+            avocadoInList = true;
+        }
+    }
+    if (!avocadoInList) {
+        std::cout << "The list does not contain Avocado" << std::endl;
+    }
+
+
+    bool durianInList = false;
+    for (int i = 0; i < listB.size(); i++) {
+        if (listB.at(i) == "Durian") {
+            std::cout << "The index of Durian in list B: " << i << std::endl;
+            durianInList = true;
+        }
+    }
+    if (!durianInList) {
+        std::cout << "The list does not contain Durian" << std::endl;
+    }
+
+
+    std::string arrFruit[] = {"Pineapple", "Passion Fruit", "Pummelo", "Melon"};
+    listB.insert(listB.begin()+4, arrFruit+1, arrFruit+3);
+
+
+    std::cout << listA.at(2) << std::endl;
+
+
+    std::cout << std::endl;
+    
     for (int i = 0; i < listA.size(); ++i) {
         std::cout << listA.at(i) << std::endl;
     }
