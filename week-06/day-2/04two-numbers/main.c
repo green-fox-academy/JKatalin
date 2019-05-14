14eadd95c1dcd5afedb345c336960697ff5edd39
Kata <Kata@1743f.w.time4vps.cloud>
2019-05-14 13:31:32 +0200

    week-06/day2 Two numbers: added

--- week-06/day-2/04two-numbers/main.c
+++ week-06/day-2/04two-numbers/main.c
@@ -3,5 +3,35 @@
 int main()
 {
     printf("Hello, World!\n");
+
+    int ad = 13 + 22;
+    printf("%d\n", ad);
+
+    int substract = 22 - 13;
+    printf("%d\n", substract);
+
+    int multy = 13 * 22;
+    printf("%d\n", multy);
+
+    float divide = 22.0 / 13.0;
+    printf("%.2f\n", divide);
+
+    int remind = 22 % 13;
+    printf("%d\n", remind);
+
     return 0;
 }
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+// Print the result of 13 added to 22
+
+// Print the result of 13 substracted from 22
+
+// Print the result of 22 multiplied by 13
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+
+// Print the reminder of 22 divided by 13
+
+// Store the results in variables and use them when you display the result
