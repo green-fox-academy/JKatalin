6bacd88d7eb21da966787509441614edb03f0a89
Kata <Kata@1743f.w.time4vps.cloud>
2019-05-26 22:43:27 +0200

    week-06/day2 Position: added

---
+++ week-06/day-2/26position/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+// create a function which takes a char array as a parameter and
+// lists all position where character 'i' is found
+
+void i_positions(char array[]);
+
+int main()
+{
+    char string[55] = "This is a string for testing";
+    //char *p;
+    i_positions(string);
+    
+    return 0;
+}
+
+void i_positions(char array[])
+{
+    for (int i = 0; i < strlen(array); i++) {
+        if (array[i] == 'i') {
+            printf("%d\n", i);
+        }
+    }
+}
